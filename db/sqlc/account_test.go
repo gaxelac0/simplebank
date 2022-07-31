dc04902921f4b7bfd9b83104203e612c057a6f6d
Gaston Axel Lacuesta <gaxelac@outlook.com>
2022-07-31 20:06:21 -0300

    random util generation + some progress into testing

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/gaxelac0/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := &CreateAccountParams{
-		Owner:    "Tom",
-		Balance:  0,
-		Currency: "ARS",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), *arg)
